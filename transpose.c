f4e9096cd689300a23b8a286b7368ad48601668e
kris1232e3 <127478981+kris1232e3@users.noreply.github.com>
2023-06-02 08:51:44 +0300

    fix mini matrix

--- transpose.c
+++ transpose.c
@@ -3,21 +3,23 @@
 
 #include "transpose.h"
 
-void allocateMatrix(float** matrix, int rows, int columns) {
-    matrix = (float**)malloc(rows * sizeof(float*));
-    for(int i = 0; i < rows; i++) {
-        matrix[i] = (float*)malloc(columns * sizeof(float));
-    }
-}
-
-// float** allocateMatrix(int rows, int columns) {
-//     float** matrix = (float**)malloc(rows * sizeof(float*));
+// void allocateMatrix(float** matrix, int rows, int columns) {
+//     matrix = (float**)malloc(rows * sizeof(float*));
 //     for(int i = 0; i < rows; i++) {
 //         matrix[i] = (float*)malloc(columns * sizeof(float));
 //     }
-//     return matrix;
 // }
 
+// private function for matrix memory allocation to prevent code repetition
+float** allocateMatrix(int rows, int columns) {
+    float** matrix = (float**)malloc(rows * sizeof(float*));
+    for(int i = 0; i < rows; i++) {
+        matrix[i] = (float*)malloc(columns * sizeof(float));
+    }
+    return matrix;
+}
+
+// private function for freeing matrix memory to prevent code repetition
 void freeMatrix(float** matrix, int rows) {
     for(int i = 0; i < rows; i++) {
         free(matrix[i]);
@@ -27,8 +29,7 @@ void freeMatrix(float** matrix, int rows) {
 
 void transpose(float** matrix, int rows, int columns) {
     // creating a temp copy of the matrix to work with
-    float** temp;
-    allocateMatrix(temp, rows, columns);
+    float** temp = allocateMatrix(rows, columns);
 
     // copying the contents of the matrix
     for(int i = 0; i < rows; i++) {
@@ -41,7 +42,7 @@ void transpose(float** matrix, int rows, int columns) {
     freeMatrix(matrix, rows);
 
     // allocationg memory for the matrix but with switched rows and columns
-    allocateMatrix(temp, columns, rows);
+    temp = allocateMatrix(columns, rows);
 
     // transpose the numbers from temp into matrix
     for(int i = 0; i < columns; i++) {
@@ -66,28 +67,25 @@ void inverseOfAMatrix(float** matrix, int rows, int columns) {
     }
     
     if(rows == 3) {
-        float** matrixOfMinors;
-        allocateMatrix(matrixOfMinors, rows, columns);
+        float** matrixOfMinors = allocateMatrix(rows, columns);
 
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < columns; j++) {
-                float** temp;
-                allocateMatrix(matrixOfMinors, rows - 1, columns - 1);
+                float** temp = allocateMatrix(rows - 1, columns - 1);
                 
                 for(int k = 0, currentRowIndex = 0; k < rows; k++) {
                     if(k == i) {
                         continue;
                     }
-                    currentRowIndex++;
 
                     for(int l = 0, currentColumnIndex = 0; l < columns; l++) {
                         if(l == j) {
                             continue;
                         }
-                        currentColumnIndex++;
 
-                        temp[currentRowIndex][currentColumnIndex] = matrix[k][l];
+                        temp[currentRowIndex][currentColumnIndex++] = matrix[k][l];
                     }
+                    currentRowIndex++;
                 }
 
                 matrixOfMinors[i][j] = determinantOfAMatrix(temp, rows - 1, columns - 1);
